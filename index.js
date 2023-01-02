63e4ab8979c256e538911520910701343fafe941
shira <shira8sara@gmail.com>
2023-01-02 14:09:35 +0200

    add public folder

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.use(express.json());
 
 app.use("/", express.static("public"));
 app.use("/api", require("./Routs/mainRouter"));
-app.use("/*", express.static("public"));
-// app.get("/*", (req,res)=>{
-// res.status(404).sendFile(process.)
-// })
+// app.use("/*", express.static("public"));
+app.get("/*", (req,res)=>{
+res.status(404).sendFile("./public/index.html")
+})
 
 app.listen(PORT, () => console.log("SERVER : connection success!"));
